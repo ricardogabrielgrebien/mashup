4aaedb93beb88fcd8187408614180459b982d163
RG <ricardo.grebien@gmail.com>
2019-02-01 08:46:55 +0100

    clear code

--- src/index.js
+++ src/index.js
@@ -27,37 +27,8 @@ class Game extends React.Component {
   }
 
 
-  loadData(url) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-         //console.log(xhttp.responseText);
-      }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
-  }
-
-
   render() {
     const { hits } = this.state;
-
-    //this.loadData("https://pokeapi.co/api/v2/pokemon/1/");
-    var resp;
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (xhttp.readyState === 4 && xhttp.status === 200) {
-         //console.log(xhttp.responseText);
-         //resp = xhttp.responseText;
-      }
-    };
-    xhttp.open("GET", "https://pokeapi.co/api/v2/pokemon/1/");
-    xhttp.send();
-
-    this.componentDidMount();
-    //console.log(xhttp.responseText);
-
 
     return (
       <ul>
